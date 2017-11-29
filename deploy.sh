5874d1bf4b8660539bc5b39cf1cae919d434de51
John Crocker <jcrocker@zenoss.com>
2017-11-29 10:11:14 -0600

    Clean up script
    + Use local variables
    + Explicitly export variables for envsubst in the relevant subshell
      instead of the global environment

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 set -e
+
 RS_YAML=rs.yaml
 SERVICE=service
 PROJECT=$(gcloud config get-value project)
@@ -9,43 +10,49 @@ log () {
 }
 
 get_deployments_by_date() {
-	MICROSERVICE=$1
+	local MICROSERVICE=$1
     kubectl get rs --sort-by=.metadata.creationTimestamp -oname --selector=microservice=$MICROSERVICE \
 		| tac | xargs -r basename -a
 }
 
 enable_deployment() {
-	log enable-deployment $1 $3
+	local DEPLOYMENT=$1
+	local SERVICE=$2
+	local VALUE=$3
+	log enable-deployment $DEPLOYMENT $VALUE
 	# set state of pods created by the rs in the future (e.g., by scaling)
-	kubectl patch rs $1 -p "'spec': {template: {metadata: {labels: {service_$2: '$3'}}}}" > /dev/null
+	kubectl patch rs $1 -p "'spec': {template: {metadata: {labels: {service_$SERVICE: '$VALUE'}}}}" > /dev/null
 	# enable/disable existing pods in the deployment
-	kubectl label pod --overwrite -l deployment=$1 service_$2=$3 > /dev/null
+	kubectl label pod --overwrite -l deployment=$DEPLOYMENT service_$SERVICE=$VALUE > /dev/null
 }
 
 get_deployment_name_from_rs() {
-	YAML_FILE=$1
+	local YAML_FILE=$1
 	python -c 'import yaml,sys; print yaml.load(sys.stdin)["metadata"]["name"]' < $YAML_FILE
 }
 
 deploy () {
-	RS_YAML_FILE=$1
-	export SERVICE_NAME=$2
-	export PROJECT_NAME=$3
+	local RS_YAML_FILE=$1
+    local SERVICE_NAME=$2
+	local PROJECT_NAME=$3
 
-	GUID=$(printf "%04x\n" $RANDOM)
-	export MICROSERVICE=$(get_deployment_name_from_rs $RS_YAML_FILE) 
-	export DEPLOYMENT=${MICROSERVICE}-${GUID}
+	local GUID=$(printf "%04x\n" $RANDOM)
+	local MICROSERVICE=$(get_deployment_name_from_rs $RS_YAML_FILE)
+	local DEPLOYMENT=${MICROSERVICE}-${GUID}
 
 	log create-deployment $DEPLOYMENT
-	RS=$(kubectl patch --local=true -oyaml \
-			-f <(envsubst < $RS_YAML_FILE) \
-			-p "$(envsubst < patch.yaml)" | \
-		 kubectl apply -f- -o name)
+
+	local ENV="SERVICE_NAME PROJECT_NAME MICROSERVICE DEPLOYMENT"
+	local RS=$(export $ENV;
+	    kubectl patch --local=true -oyaml \
+		    -f <(envsubst < $RS_YAML_FILE) \
+		    -p "$(envsubst < patch.yaml)" | \
+		kubectl apply -f- -o name)
 
 	# Wait for pods to be ready
 	log wait-for-deployment
 	while true ; do
-		expr=$(kubectl get $RS -o 'go-template=[ "{{.status.readyReplicas}}" = "{{.status.replicas}}" ]')
+		local expr=$(kubectl get $RS -o 'go-template=[ "{{.status.readyReplicas}}" = "{{.status.replicas}}" ]')
 		eval $expr && break
 		sleep 1
 	done
@@ -53,14 +60,16 @@ deploy () {
 }
 
 do_test () {
-	export DEPLOYMENT=$1
-	export PROJECT_NAME=$2
+	local DEPLOYMENT=$1
+	local PROJECT_NAME=$2
+
 	log test-deployment $DEPLOYMENT
-	POD=$(kubectl create -f <(envsubst < test.yaml) -oname)
+	local ENV="DEPLOYMENT PROJECT_NAME"
+	local POD=$(kubectl create -f <(export $ENV; envsubst < test.yaml) -oname)
 
 	log wait-for-test $POD
 	while true ; do
-		status=$(kubectl get $POD -o 'go-template={{.status.phase}}')
+		local status=$(kubectl get $POD -o 'go-template={{.status.phase}}')
 		[ "$status" = "Succeeded" ] && return 0
 		[ "$status" = "Failed" ] && return 1
 		sleep 3
