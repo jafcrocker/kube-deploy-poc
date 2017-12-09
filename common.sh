42c2d7c1c5e75c1adf8fd25624996ae31d7f3afa
John Crocker <jcrocker@zenoss.com>
2017-12-09 12:26:31 -0600

    First commit on canary

--- common.sh
+++ common.sh
@@ -1,8 +1,4 @@
 #! /usr/bin/env bash
-set -e
-RS_YAML=rs.yaml
-SERVICE=service
-PROJECT=$(gcloud config get-value project)
 
 log () {
     echo $* >&2
@@ -12,7 +8,8 @@ get_deployments_by_date() {
     local RS_YAML_FILE=$1
     local MICROSERVICE=$(get_deployment_name_from_rs $RS_YAML_FILE)
     kubectl get rs --sort-by=.metadata.creationTimestamp -oname --selector=microservice=$MICROSERVICE \
-        | tac | xargs -r basename -a
+        | tail -r | xargs -r basename -a
+    exit 1
 }
 
 enable_deployment() {
@@ -59,34 +56,4 @@ deploy () {
     echo $RS
 }
 
-do_test () {
-    local DEPLOYMENT=$1
-    local PROJECT_NAME=$2
-
-    log test-deployment $DEPLOYMENT
-    local ENV="DEPLOYMENT PROJECT_NAME"
-    local POD=$(kubectl create -f <(export $ENV; envsubst < test.yaml) -oname)
-
-    log wait-for-test $POD
-    while true ; do
-        local status=$(kubectl get $POD -o 'go-template={{.status.phase}}')
-        [ "$status" = "Succeeded" ] && return 0
-        [ "$status" = "Failed" ] && return 1
-        sleep 3
-    done
-}
-
-LAST_DEPLOYMENT=$(get_deployments_by_date $RS_YAML | head -1)
-
-DEPLOYMENT=$(basename $(deploy $RS_YAML $SERVICE $PROJECT))
-
-[ "$LAST_DEPLOYMENT" = "" ] || enable_deployment $LAST_DEPLOYMENT $SERVICE false
-enable_deployment $DEPLOYMENT $SERVICE true
-
-if do_test $DEPLOYMENT $PROJECT ; then
-    [ "$LAST_DEPLOYMENT" = "" ] || kubectl delete rs $LAST_DEPLOYMENT
-else
-    [ "$LAST_DEPLOYMENT" = "" ] || enable_deployment $LAST_DEPLOYMENT $SERVICE true
-    kubectl delete rs $DEPLOYMENT
-fi
 
