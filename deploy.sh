0eb953837618c9fef22fcd7c1144150713f91408
John Crocker <jcrocker@zenoss.com>
2017-12-15 13:22:07 -0600

    Specify rs yaml on command line for deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 set -e
-RS_YAML=${RS_YAML:=rs.yaml}
+RS_YAML=${1:-rs.yaml}
 SERVICE=${SERVICE:=service}
 PROJECT=${PROJECT:=$(gcloud config get-value project)}
 STACK=${STACK:=staging}
